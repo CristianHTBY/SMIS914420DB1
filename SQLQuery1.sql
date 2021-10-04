f3b0eb337fa94ab89ea36727d764fafc6559286c
unknown <alavaradocristian670@gmail.com>
2021-10-04 10:29:56 -0600

    se mudifico la vista

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -6,3 +6,40 @@ SELECT
 FROM Products P
 INNER JOIN Categories c
 ON p.CategoryID = c.CategoryID;
+
+--crear una vista llamada ListadoProductos que muestre 
+--el nombre del producto, categoria y proveedor
+--ordenados por nombre de gategoria 
+
+SELECT 
+	p.ProductName producto,
+	c.CategoryName categoria,
+	s.SupplierID proveedor,
+	s.ContactName,
+	o.OrderID
+	
+	
+FROM 
+	Products P
+INNER JOIN Categories c ON P.CategoryID = C.CategoryID
+INNER JOIN Suppliers s ON p. SupplierID = s.SupplierID
+LEFT JOIN [Order Details] o ON p.ProductID = o.ProductID
+
+SELECT 
+	p.ProductName producto,
+	c.CategoryName categoria,
+	s.SupplierID proveedor,
+	s.ContactName,
+	o.OrderID
+	
+	
+FROM 
+	Products P
+FULL JOIN Categories c ON P.CategoryID = C.CategoryID
+FULL JOIN Suppliers s ON p. SupplierID = s.SupplierID
+FULL JOIN [Order Details] o ON p.ProductID = o.ProductID
+
+
+
+
+
